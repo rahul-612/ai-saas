c5d3a575b6dfa5b6593420a4027c0a7a59ea30b0
rahul <rk785164@gmail.com>
2023-07-26 15:36:06 +0530

    fixed conversation page error

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,5 +1,7 @@
 import Stripe from "stripe"
 
+export const dynamic = "force-dynamic";
+
 export const stripe = new Stripe(process.env.STRIPE_API_KEY!, {
   apiVersion: "2022-11-15",
   typescript: true,
