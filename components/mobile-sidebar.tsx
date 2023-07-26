17dd3081c859f2a3df023f61ed82d42b8c577a93
rahul <rk785164@gmail.com>
2023-07-26 15:29:53 +0530

    fixed conversation page error

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -8,11 +8,11 @@ import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { Sidebar } from "@/components/sidebar";
 
 export const MobileSidebar = ({
-  // apiLimitCount = 0,
-  // isPro = false
+  apiLimitCount = 0,
+  isPro = false
 }: {
-  // // apiLimitCount: number;
-  // isPro: boolean;
+  apiLimitCount: number;
+  isPro: boolean;
 }) => {
   const [isMounted, setIsMounted] = useState(false);
 
@@ -32,8 +32,7 @@ export const MobileSidebar = ({
         </Button>
       </SheetTrigger>
       <SheetContent side="left" className="p-0">
-       
-        <Sidebar  />
+      <Sidebar isPro={isPro} apiLimitCount={apiLimitCount} />
       </SheetContent>
     </Sheet>
   );
